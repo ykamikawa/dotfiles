b65251f6c03447ac0713b15464df2922dcbb3d53
ykamikawa <27678705+ykamikawa@users.noreply.github.com>
2020-04-18 05:20:22 +0900

    Add starship

---
+++ zsh/install_plugins.sh
@@ -0,0 +1,4 @@
+#! /bin/bash
+git clone https://github.com/zsh-users/zsh-autosuggestions ~/.oh-my-zsh/custom/plugins/zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ~/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting
+git clone https://github.com/zsh-users/zsh-completions.git ~/.oh-my-zsh/custom/plugins/zsh-completions
