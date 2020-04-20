17c131bd45d170d7893b75e17686101ae2ddfedb
ykamikawa <27678705+ykamikawa@users.noreply.github.com>
2020-04-20 16:52:50 +0900

    Add starship

--- zsh/install.sh
+++ zsh/install.sh
@@ -6,9 +6,16 @@ source ~/.zshrc
 git clone https://github.com/zsh-users/zsh-syntax-highlighting ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 git clone https://github.com/zsh-users/zsh-completions ${ZSH_CUSTOM:=~/.oh-my-zsh/custom}/plugins/zsh-completions
+# neovim
+sudo apt install software-properties-common
+sudo -E add-apt-repository ppa:neovim-ppa/unstable
+sudo apt update
+sudo apt install neovim
 # pyenv
+rm -rf ~/.pyenv
 git clone https://github.com/yyuu/pyenv.git ~/.pyenv
 # goenv
+rm -rf ~/.goenv
 git clone https://github.com/syndbg/goenv.git ~/.goenv
 
 # init
