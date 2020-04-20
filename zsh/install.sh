a7fab0010ad5840900474e8866daddb8e6c62c3a
ykamikawa <27678705+ykamikawa@users.noreply.github.com>
2020-04-20 16:58:42 +0900

    Add starship

--- zsh/install.sh
+++ zsh/install.sh
@@ -6,19 +6,32 @@ source ~/.zshrc
 git clone https://github.com/zsh-users/zsh-syntax-highlighting ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 git clone https://github.com/zsh-users/zsh-completions ${ZSH_CUSTOM:=~/.oh-my-zsh/custom}/plugins/zsh-completions
+
 # neovim
-sudo apt install software-properties-common
+export DEBIAN_FRONTEND=noninteractive
+sudo -E apt install software-properties-common
 sudo -E add-apt-repository ppa:neovim-ppa/unstable
-sudo apt update
-sudo apt install neovim
+sudo -E apt update
+sudo -E apt install neovim
+
 # pyenv
 rm -rf ~/.pyenv
 git clone https://github.com/yyuu/pyenv.git ~/.pyenv
+
 # goenv
 rm -rf ~/.goenv
 git clone https://github.com/syndbg/goenv.git ~/.goenv
 
 # init
 rm ~/.zshrc
-ln -s ~/~/dotfiles/zsh/..zshrc ~/.zshrc
+ln -s ~/dotfiles/zsh/.zshrc ~/.zshrc
 source ~/.zshrc
+
+# python
+pyenv install miniconda3-4.3.30
+pyenv global miniconda3-4.3.30
+pip install neovim
+
+# go
+goenv install 1.9.7
+goenv global 1.9.7
