60f3b1443f583e0163deadc66cbd857d5e16d823
ykamikawa <yuta.kamikawa.002@layerx.co.jp>
2024-04-09 05:43:39 +0000

    Fix

--- dev/install.sh
+++ dev/install.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 # AWS CLI
-curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"jh
+curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
 unzip awscliv2.zip
-./aws/install
+sudo ./aws/install
 
 # docker-compose
 curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose && \
