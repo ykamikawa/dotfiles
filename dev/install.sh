f560c4c9b6ff0f022dd1b52ba68fc829a7da87c3
ykamikawa <yuta.kamikawa.002@layerx.co.jp>
2024-09-11 10:19:21 +0000

    fix

--- dev/install.sh
+++ dev/install.sh
@@ -8,15 +8,15 @@ rm -rf awscliv2.zip
 rm -rf ./aws
 
 # docker-compose
-curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose && \
-chmod +x /usr/local/bin/docker-compose
+curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose &&
+	chmod +x /usr/local/bin/docker-compose
 
 sudo apt update
 sudo apt install -y \
-  build-essential libssl-dev libsqlite3-dev libbz2-dev libgdbm-dev libncurses5-dev \
-  libncursesw5-dev libreadline-dev zlib1g-dev libffi-dev liblzma-dev \
-  libjpeg-dev libpng-dev libtiff-dev \
-  lua5.1 luarocks zsh
+	build-essential libssl-dev libsqlite3-dev libbz2-dev libgdbm-dev libncurses5-dev \
+	libncursesw5-dev libreadline-dev zlib1g-dev libffi-dev liblzma-dev \
+	libjpeg-dev libpng-dev libtiff-dev \
+	lua5.1 luarocks zsh
 
 # Neovim
 sudo curl -L https://github.com/neovim/neovim/releases/download/v0.9.5/nvim-linux64.tar.gz -o nvim.tar.gz
@@ -33,11 +33,8 @@ PYENV_ROOT=~/.pyenv
 PATH=$PATH:$PYENV_ROOT/shims:$PYENV_ROOT/bin:~/.local/bin
 eval "$(pyenv init -)"
 luarocks --local --lua-version=5.1 install magick
-pyenv install 3.11.9
-pyenv global 3.11.9
-# pip install pynvim jupyter_client jupytext jupyterlab
-# poetry
-curl -sSL https://install.python-poetry.org | python3 - --version 1.5.1
+pyenv install 3.10.11
+pyenv global 3.10.11
 
 # mecab, ripgrep
 sudo apt-get update
@@ -54,12 +51,12 @@ goenv global 1.18.5
 
 # nodenv, node-buildプラグインのインストール
 git clone https://github.com/nodenv/nodenv.git ~/.nodenv
-cd ~/.nodenv && src/configure && make -C src && \
-mkdir -p ~/.nodenv/plugins && \
-git clone https://github.com/nodenv/node-build.git ~/.nodenv/plugins/node-build
+cd ~/.nodenv && src/configure && make -C src &&
+	mkdir -p ~/.nodenv/plugins &&
+	git clone https://github.com/nodenv/node-build.git ~/.nodenv/plugins/node-build
 PATH=~/.nodenv/bin:~/.nodenv/shims:${PATH}
 eval "$(nodenv init -)"
-nodenv install -y 20.10.0
+nodenv install 20.10.0
 nodenv global 20.10.0
 
 # Clone dotfiles and nvim settings
